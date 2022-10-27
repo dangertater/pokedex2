36025f594a200b1a5dfa9cc30597d5ccf01e0bd1
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-27 10:16:15 -0600

    fetch working, difficulty getting 150, working with 2 arrays equaling 40ish

--- scripts.js
+++ scripts.js
@@ -1,18 +1,26 @@
 // let body = document.getElementById("body")
+let freshJSONObjects = []
 let initialFetchArray = []
 let firstGenObjectsArray = []
 //will fetch the api information
 
 //q4e reference 'how are they different' at bottom of page
-let fetchPokedexAPI = async () => {
-	const res = await fetch("https://pokeapi.co/api/v2/pokemon")
+//q4e struggling on getting it to recursively call in order to get next url, currently does once
+let fetchPokedexAPI = async (url) => {
+	if (freshJSONObjects.length > 1) {
+		return null
+	}
+	let res = await fetch(url)
 	let data = await res.json()
-	console.log(data)
+	freshJSONObjects.push(data)
+	let nextURL = freshJSONObjects[0].next
+	fetchPokedexAPI(nextURL)
+	console.log(freshJSONObjects)
 }
 
 // let JSONthis = (res) => {}
 
-fetchPokedexAPI()
+fetchPokedexAPI("https://pokeapi.co/api/v2/pokemon")
 // then((res) => res.json()).then()
 
 //q4e how are they different?
