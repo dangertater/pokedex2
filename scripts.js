2869469b9be79ce3c10d10a712255410cecf05b2
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-27 09:00:18 -0600

    tinkering to learn git

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-let body = document.getElementById("body")
+// let body = document.getElementById("body")
+let initialFetchArray = []
+let firstGenObjectsArray = []
+//will fetch the api information
+let fetchPokedexAPI = async () => {
+	setTimeout(() => {
+		fetch("https://pokeapi.co/api/v2/pokemon")
+	}, 3000)
+}
+
+let consoleLogTest = async () => {
+	await fetchPokedexAPI()
+	console.log("consoleLogTest", "completed")
+}
+
+// fetchPokedexAPI("https://pokeapi.co/api/v2/pokemon")
+consoleLogTest()
