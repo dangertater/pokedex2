1df3033d8d6f4c97c4a9cabcdbcfb5293726aafb
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-27 09:46:54 -0600

    fetch and json working, conceptual q4e added at bottom for

--- scripts.js
+++ scripts.js
@@ -2,16 +2,33 @@
 let initialFetchArray = []
 let firstGenObjectsArray = []
 //will fetch the api information
+
+//q4e reference 'how are they different' at bottom of page
 let fetchPokedexAPI = async () => {
-	setTimeout(() => {
-		fetch("https://pokeapi.co/api/v2/pokemon")
-	}, 3000)
+	const res = await fetch("https://pokeapi.co/api/v2/pokemon")
+	let data = await res.json()
+	console.log(data)
 }
 
-let consoleLogTest = async () => {
-	await fetchPokedexAPI()
-	console.log("consoleLogTest", "completed")
-}
+// let JSONthis = (res) => {}
 
-// fetchPokedexAPI("https://pokeapi.co/api/v2/pokemon")
-consoleLogTest()
+fetchPokedexAPI()
+// then((res) => res.json()).then()
+
+//q4e how are they different?
+//----let fetchPokedexAPI = async () => {
+//----const res = await fetch("https://pokeapi.co/api/v2/pokemon")
+//----let data = await res.json()
+//----console.log(data)
+//----}
+//----VS
+//----let fetchPokedexAPI = async () => {
+//----const res = await fetch("https://pokeapi.co/api/v2/pokemon")
+//----console.log(res.json())
+//----}
+//----
+//----
+//----
+//----
+//----
+//----
